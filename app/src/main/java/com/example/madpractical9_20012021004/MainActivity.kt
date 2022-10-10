e4ad5a79ef157432ffd83433a633f3fc4c4c5cd1
Student <neehopa@gmail.com>
2022-10-10 12:39:51 +0530

    Practical-9 Started

---
+++ app/src/main/java/com/example/madpractical9_20012021004/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.madpractical9_20012021004
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+
+class MainActivity : AppCompatActivity() {
+    val SMS_PERMISSION_CODE = 110
+    private fun requestSMSPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_SMS)){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_SMS,android.Manifest.permission.SEND_SMS),SMS_PERMISSION_CODE)
+        }
+    }
+    private val isSMSReadPermission:Boolean
+    get() = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
+    private val isSMSWritePermission:Boolean
+    get() = ContextCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
